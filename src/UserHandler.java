7ade172fef11e859136b8a1572fa175cb42b1b10
Saman AMP <samanamp@outlook.com>
2013-10-25 10:54:46 +1100

    User Handler added

---
+++ src/UserHandler.java
@@ -0,0 +1,17 @@
+
+public class UserHandler {
+
+	private DBHandler dbHandler;
+	
+	public UserHandler(){
+		dbHandler = new DBHandler();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
