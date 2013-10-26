64761fb99b65082fd8069fc356858bcaa7f196dc
Cesar Martinez D Granda <checharo@gmail.com>
2013-10-26 17:16:44 +1100

    Included users in listgroups

--- src/servlets/GroupServlet.java
+++ src/servlets/GroupServlet.java
@@ -22,6 +22,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import data.Group;
+import data.Membership;
 import data.Message;
 import data.UserData;
 import exceptions.CustomException;
@@ -134,6 +135,14 @@ public class GroupServlet extends HttpServlet {
 				jgroup.put("name", group.getName());
 				jgroup.put("owner", group.getOwner());
 				jgroup.put("id", group.get_id());
+				
+				ArrayList<Membership> users = group.getUsers();
+				JSONArray jusers = new JSONArray();
+				for (Membership user : users) {
+					jusers.add(user.getEmail());
+				}
+				jgroup.put("users", jusers);
+				
 				jgroups.add(jgroup);
 			}			
 			res.put("success", true);
